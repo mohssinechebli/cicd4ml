61b02d1b4e5b9d9dc10d21700e64d7c5348453ad
Mohssine Chebli <64640957+mohssinechebli@users.noreply.github.com>
2024-10-24 00:48:50 +0200

    Delete hello_wrold.py

--- hello_wrold.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-def main():
-  print("Hello, World!")
-  
-  # Get the current time and print it
-  current_time = datetime.datetime.now()
-  print("Current time:", current_time)
-if __name__ = main()
